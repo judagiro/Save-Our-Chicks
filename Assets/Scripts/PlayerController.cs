d6821b628fd3be466be1e40a854a5507b1f90382
Julián David Giraldo Roldán <Julin David Giraldo Roldn>
2016-10-02 20:24:58 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(x, y);
+	
+	}
+
+    private void move()
+    {
+        this.
+    }
+}
