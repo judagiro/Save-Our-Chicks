3994dfb3afa741258c1d985ca13a2c7010ec17b0
Julián David Giraldo Roldán <Julin David Giraldo Roldn>
2016-10-03 02:18:20 -0400

    added Bullets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,26 +3,54 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public GameObject Bullet;
+    public GameObject BulletPosition;
+
     public float speed;
+    private Transform _Transform;
 
 	// Use this for initialization
 	void Start ()
     {
-
+        
 	
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject Bullet1 = (GameObject)Instantiate(Bullet);
+            Bullet1.transform.position = BulletPosition.transform.position;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        Vector2 direction = new Vector2(x, y);
-	
-	}
+        Vector2 direction = new Vector2(x, y).normalized;
 
-    private void move()
+        move(direction);      
+
+
+
+    }
+
+    private void move(Vector2 direction)
     {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        max.x = max.x - 0.225f;
+        min.x = min.x - 0.225f;
+
+        Vector2 pos = transform.position;
+        pos += direction * speed*Time.deltaTime;
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+
+        transform.position = pos;
         
+
     }
 }
